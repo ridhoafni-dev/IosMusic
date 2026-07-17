6318781e6770c61a4c6bf9ec8a5c138ef03dd393
ridhoafni-dev <ridhoafni.dev@gmail.com>
2026-07-17 22:45:30 +0700

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by User on 17/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
